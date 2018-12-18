993b25d540f26fbfad07c51a803078fd89d605fd
Agustinus Gladyawan Dwi Putra <45622674+GladyPutra@users.noreply.github.com>
2018-12-18 22:49:09 +0700

    Project

---
+++ the way/code/Assets/Scripts/Finish.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Finish : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+	public void PlayAgain()
+	{
+
+		Application.LoadLevel(4);
+
+	}
+
+	public void SelectLevel()
+	{
+
+		Application.LoadLevel(7);
+
+	}
+
+	public void MainMenu()
+	{
+
+		Application.LoadLevel(1);
+
+	}
+}
---
+++ the way/code/Assets/Scripts/Finish2.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish2 : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+	{
+
+		Application.LoadLevel (9);
+
+	}
+}
